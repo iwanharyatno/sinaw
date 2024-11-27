7327f5c364b13e77edc8182e8ddbd40cebeb1556
DinandCode <dinandaristo16@gmail.com>
2024-11-27 13:49:17 +0700

    halaman login

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,6 +7,8 @@
     <title>@yield('title')</title>
     @vite('resources/css/app.css')
     @stack('head')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
 </head>
 <body>
     @yield('content')
