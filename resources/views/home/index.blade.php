29a6c28dcd847ac4dfd6dae152e9761335232ac5
iwanharyatno <iwnharry61@gmail.com>
2024-11-27 13:03:09 +0700

    add custom fonts

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Home | SINAW')
 
 @section('content')
-<h1 class="text-3xl font-bold text-blue-700 text-center">Lorem Ipsum</h1>
+<div class="text-white bg-gradient-to-b from-[#3A4561] to-[#0B1735] min-h-screen">
+    <nav>
+        <ul class="p-4 flex justify-between gap-4 items-center font-inter">
+            <li><h1 class="text-2xl font-bold font-fredoka">SinaW</h1></li>
+            <li class="ml-auto"><a href="/login" class="hover:underline">Login</a></li>
+            <li><a href="/register" class="inline-block px-4 py-2 bg-[#B9295A] rounded-md">Daftar</a></li>
+        </ul>
+    </nav>
+</div>
 @endsection
