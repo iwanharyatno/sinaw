181b940b2247ee669623aa70e84b3cc2d6f64796
DinandCode <dinandaristo16@gmail.com>
2024-12-14 22:54:36 +0700

    gambar close

--- resources/views/kuis/close.blade.php
+++ resources/views/kuis/close.blade.php
@@ -8,7 +8,7 @@
 
     <div class="text-center p-8 bg-gray-900 font-poppins text-white rounded-lg shadow-lg max-w-md">
                 <!-- Ilustrasi atau Icon -->
-                <img src="/image/closed_quiz.svg" alt="Kuis Ditutup" class="w-32 mx-auto mb-4 animate-bounce" />
+                <img src="{{ asset('/asset/close.png')}}" alt="Kuis Ditutup" class="w-20 mx-auto mb-4 animate-bounce" />
     
                 <!-- Pesan Utama -->
                 <h1 class="text-2xl font-bold mb-2">Oops! Kuis ini sudah ditutup.</h1>
