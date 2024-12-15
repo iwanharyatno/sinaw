34e865267e5b28bb927873e0caa4cb70f780aa8d
iwanharyatno <iwnharry61@gmail.com>
2024-12-15 11:34:07 +0700

    removing some files

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,7 +8,6 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     @vite('resources/css/app.css')
-    <script src="{{ asset('scripts/avataaars.js') }}"></script>
     @stack('head')
 </head>
 <body>
